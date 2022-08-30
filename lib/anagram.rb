c13cdd8699c5cd78481caf10989e567c60b64537
Zachary Dean <Zakedoubled@gmail.com>
2022-08-30 12:29:12 -0400

    commit tree c8d8fe8d65b66364fd55187ecc5e53051aca8ae0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+    def initialize word
+        @word = word
+    end
+
+    def match array
+        new_arr = array.filter {|string| 
+        string.chars.sort == @word.chars.sort}
+        new_arr
+    end
+
+end
